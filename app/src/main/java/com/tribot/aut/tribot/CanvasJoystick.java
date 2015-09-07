c6670b3fcd99a4efcf204d5c3ed5dc1c7bd2cd44
Ricky Dua <rickx342@gmail.com>
2015-09-08 00:35:01 +1200

    trying making multitouch work properly

--- app/src/main/java/com/tribot/aut/tribot/CanvasJoystick.java
+++ app/src/main/java/com/tribot/aut/tribot/CanvasJoystick.java
@@ -21,9 +21,9 @@ public class CanvasJoystick extends View implements View.OnTouchListener {
     boolean isPushedDown[], positiveY, positiveX;
     Bitmap blueJoystick;
     Point[] points;
-    float x, y, c, angle;
+    float angle;
     String position = "";
-
+    int centerX, centerY,centerX2, centerY2;
 
     public static final int JOYSTICK_RADIUS = 255;
 
@@ -61,130 +61,124 @@ public class CanvasJoystick extends View implements View.OnTouchListener {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        int pwmValue= 0;
-        int cx = JOYSTICK_RADIUS;
-        int cy = canvas.getHeight()-JOYSTICK_RADIUS;
+        int pwmValue = 0;
 
-        int cx2 = canvas.getWidth() - JOYSTICK_RADIUS;
-        int cy2 = canvas.getHeight() - JOYSTICK_RADIUS;
+        centerX = JOYSTICK_RADIUS;
+        centerY = canvas.getHeight() - JOYSTICK_RADIUS;
+        centerX2 = canvas.getWidth() - JOYSTICK_RADIUS;
+        centerY2 = canvas.getHeight() - JOYSTICK_RADIUS;
         super.onDraw(canvas);
         canvas.drawColor(Color.BLACK);
         paint.setColor(Color.rgb(115, 215, 234));
 
-        //Draw two Circles, used for restricting joystick to a certain distance from the radius of the circle.
-        canvas.drawCircle(cx, cy, JOYSTICK_RADIUS, paint);
-        canvas.drawCircle(cx2, cy2, JOYSTICK_RADIUS, paint);
+        //Draw two Circles, used for restricting joystick to a certain distance from the radius
+        // of the circle.
+        canvas.drawCircle(centerX, centerY, JOYSTICK_RADIUS, paint);
+        canvas.drawCircle(centerX2, centerY2, JOYSTICK_RADIUS, paint);
         paint.setColor(Color.WHITE);
         canvas.drawLine(canvas.getWidth() / 2, 0, canvas.getWidth() / 2, canvas.getHeight(), paint);
-        paint.setTextSize(20);
+
         //draw text for debugging
+        paint.setTextSize(20);
         canvas.drawText("x1 :" + points[0].x, 10, 20, paint);
         canvas.drawText("y1 :" + points[0].y, 10, 40, paint);
         canvas.drawText("x2 :" + points[1].x, 10, 60, paint);
         canvas.drawText("y2 :" + points[1].y, 10, 80, paint);
         canvas.drawText("angle" + angle, 10, 100, paint);
-        canvas.drawText("hyp" + c, 10, 120, paint);
         canvas.drawText("positiveX: " + positiveX + " positiveY " + positiveY, 10, 140, paint);
         canvas.drawText("position: " + position, 10, 160, paint);
 
+        //If any touch event occurs within a specific bound, draw a corresponding joystick.
 
-        //If any touch event occurs, draw a corresponding joystick.
-        //***Left Joystick***
+        //call calculateArc to calculate points to limit the joystick
         if (isPushedDown[0]) {
-            //call calculateArc to calculate points to limit the joystick
-            calculateArc(JOYSTICK_RADIUS - (blueJoystick.getWidth() / 2), canvas.getHeight() - JOYSTICK_RADIUS - (blueJoystick.getHeight() / 2),
-
-
-                    (points[0].x - (blueJoystick.getWidth() / 2)), (points[0].y - (blueJoystick.getHeight() / 2)));
-
-            canvas.drawBitmap(blueJoystick, x, y, null);
-
+           // if (points[0].x < centerX + (JOYSTICK_RADIUS+extraSpace) && points[0].y > centerY - (JOYSTICK_RADIUS+extraSpace)) {
+                Point restrictionPoint = calculateArc((blueJoystick.getWidth() / 2), centerY - (blueJoystick.getHeight() / 2),
+                        (points[0].x - (blueJoystick.getWidth() / 2)), (points[0].y - (blueJoystick.getHeight() / 2)));
+                canvas.drawBitmap(blueJoystick, restrictionPoint.x, restrictionPoint.y, null);
+           /* } else
+                canvas.drawBitmap(blueJoystick, centerX - (blueJoystick.getWidth() / 2), centerY - (blueJoystick.getHeight() / 2), null);*/
         }
-        else {
-            canvas.drawBitmap(blueJoystick, JOYSTICK_RADIUS - (blueJoystick.getWidth() / 2), canvas.getHeight() - JOYSTICK_RADIUS - (blueJoystick.getHeight() / 2), null);
-        }
+        else
+            canvas.drawBitmap(blueJoystick, centerX - (blueJoystick.getWidth() / 2), centerY - (blueJoystick.getHeight() / 2), null);
 
-        //***Right Joystick***
+
         if (isPushedDown[1]) {
+            //if(points[1].x > centerX2 - (JOYSTICK_RADIUS+extraSpace) && points[1].y > centerY2 - (JOYSTICK_RADIUS+extraSpace)) {
+                Point restrictionPoint = calculateArc(centerX2 - (blueJoystick.getWidth() / 2),
+                        centerY2 - (blueJoystick.getHeight() / 2),
+                        (points[1].x - (blueJoystick.getWidth() / 2)),
+                        (points[1].y - (blueJoystick.getHeight() / 2)));
+                canvas.drawBitmap(blueJoystick, restrictionPoint.x, restrictionPoint.y, null);
+           /* }else
+                canvas.drawBitmap(blueJoystick, centerX2 - (blueJoystick.getWidth() / 2), centerY2 - (blueJoystick.getHeight() / 2), null);*/
+        } else
+            canvas.drawBitmap(blueJoystick, centerX2 - (blueJoystick.getWidth() / 2), centerY2 - (blueJoystick.getHeight() / 2), null);
 
-            calculateArc(canvas.getWidth() - JOYSTICK_RADIUS - (blueJoystick.getWidth() / 2),
-                    canvas.getHeight() - JOYSTICK_RADIUS - (blueJoystick.getHeight() / 2),
-                    (points[1].x - (blueJoystick.getWidth() / 2)),
-                    (points[1].y - (blueJoystick.getHeight() / 2)));
 
-            canvas.drawBitmap(blueJoystick, x, y, null);
-        } else {
-            canvas.drawBitmap(blueJoystick, canvas.getWidth() - JOYSTICK_RADIUS - (blueJoystick.getWidth() / 2), canvas.getHeight() - JOYSTICK_RADIUS - (blueJoystick.getHeight() / 2), null);
-        }
-        pwmValue = Math.abs((int)Math.sqrt(Math.pow(points[0].x-cx,2)+Math.pow(points[0].y-cy,2)));
-        System.out.println(pwmValue);
+
+        pwmValue = Math.abs((int) Math.sqrt(Math.pow(points[0].x - centerX, 2) + Math.pow(points[0].y - centerY, 2)));
     }
-
 
     //This method calculates the values in an arc, with the centre of the arc at xcentre/ycentre
     //and x1 and y1 are the values to be calculated
-    public void calculateArc(float xcentre, float ycentre, float x1, float y1) {
-        x = x1 - xcentre;
-        y = y1 - ycentre;
+    public Point calculateArc(float xcentre, float ycentre, float x1, float y1) {
+        float finalX = x1 - xcentre;
+        float finalY = y1 - ycentre;
         //Calculate angle by using Trigonometry and hypotenuse by using Pythagoras' theorem
-        angle = (float) Math.atan(Math.abs(y / x));
-        c = FloatMath.sqrt(x * x + y * y);
-        //Figure out whether the x and y are positive or negative
-        positiveY = y < 0;
-        positiveX = x > 0;
-
+        angle = (float) Math.atan(Math.abs(finalY / finalX));
+        float hyp = FloatMath.sqrt(finalX * finalX + finalY * finalY);
+        //Figure out whether the finalX and finalY are positive or negative
+        positiveY = finalY < 0;
+        positiveX = finalX > 0;
         getDirection(angle);
 
-        System.out.println("angle: " + angle + " hypotenuse: " + c + " positiveY: " + positiveY + "  " + y + "position: " + position);
-
         //If the length of the hyp is greater than the radius of the joystick then limit the joystick
-        if (c > JOYSTICK_RADIUS) {
-
+        if (hyp > JOYSTICK_RADIUS) {
             //This code will limit the joystick according to the corresponding quadrants in the circle.
-            if (x > 0 && y > 0) {//bottom right limitation
+            if (finalX > 0 && finalY > 0) {//bottom right limitation
                 x1 = (xcentre + (JOYSTICK_RADIUS * FloatMath.cos(angle)));
                 y1 = (ycentre + (JOYSTICK_RADIUS * FloatMath.sin(angle)));
-            } else if (x > 0 && y < 0) {//top right limitation
+            } else if (finalX > 0 && finalY < 0) {//top right limitation
                 x1 = (xcentre + (JOYSTICK_RADIUS * FloatMath.cos(angle)));
                 y1 = (ycentre - (JOYSTICK_RADIUS * FloatMath.sin(angle)));
-            } else if (x < 0 && y < 0) {//top left limitation
+            } else if (finalX < 0 && finalY < 0) {//top left limitation
                 x1 = (xcentre - (JOYSTICK_RADIUS * FloatMath.cos(angle)));
                 y1 = (ycentre - (JOYSTICK_RADIUS * FloatMath.sin(angle)));
-            } else if (x < 0 && y > 0) {//bottom left limitation
+            } else if (finalX < 0 && finalY > 0) {//bottom left limitation
                 x1 = (xcentre - (JOYSTICK_RADIUS * FloatMath.cos(angle)));
                 y1 = (ycentre + (JOYSTICK_RADIUS * FloatMath.sin(angle)));
             }
-
         } else {
-            x1 = xcentre + x;
-            y1 = ycentre + y;
+            x1 = xcentre + finalX;
+            y1 = ycentre + finalY;
         }
-
-        x = x1;
-        y = y1;
-
+        finalX = x1;
+        finalY = y1;
+        return new Point((int)finalX,(int)finalY);
     }
 
-    //This method will get the direction of the joystick by using the angle in radiansdded  and whether the values are positive and negative
+    //This method will get the direction of the joystick by using the angle in radiansdded
+    // and whether the values are positive and negative
     public void getDirection(float angle) {
         //get the 4 cardinal points of the joystick
-        if (positiveY == true && angle >= 1.047) {
+        if (positiveY && angle >= 1.047) {
             position = "UP";
         } else if (!positiveY && angle >= 1.047) {
             position = "DOWN";
-        } else if (positiveX == false && angle <= 0.523) {
+        } else if (!positiveX && angle <= 0.523) {
             position = "LEFT";
-        } else if (positiveX == true && angle <= 0.523) {
+        } else if (positiveX && angle <= 0.523) {
             position = "RIGHT";
         }
         //get the 4 other intercardinal points of the joystick
-        else if ((positiveX == true && positiveY == true) && (angle <= 1.047 && angle >= 0.523)) {
+        else if ((positiveX && positiveY) && (angle <= 1.047 && angle >= 0.523)) {
             position = "UP & RIGHT";
-        } else if ((positiveX == false && positiveY == true) && (angle <= 1.047 && angle >= 0.523)) {
+        } else if ((!positiveX && positiveY) && (angle <= 1.047 && angle >= 0.523)) {
             position = "UP & LEFT";
-        } else if ((positiveX == true && positiveY == false) && (angle <= 1.047 && angle >= 0.523)) {
+        } else if ((positiveX && !positiveY) && (angle <= 1.047 && angle >= 0.523)) {
             position = "DOWN & RIGHT";
-        } else if ((positiveX == false && positiveY == false) && (angle <= 1.047 && angle >= 0.523)) {
+        } else if ((!positiveX && !positiveY) && (angle <= 1.047 && angle >= 0.523)) {
             position = "DOWN & LEFT";
         }
     }
@@ -193,21 +187,43 @@ public class CanvasJoystick extends View implements View.OnTouchListener {
     public boolean onTouch(View v, MotionEvent event) {
         //Go through all the touch events i.e. pointers, setting @isPushedDown[i] to false or true
         //Depending on the touch event.
+        int extraSpace = 400;
+        /*if (event.getX(0) < centerX + (JOYSTICK_RADIUS+extraSpace) && event.getY(0) > centerY -
+                (JOYSTICK_RADIUS+extraSpace) || event.getX(1) < centerX + (JOYSTICK_RADIUS+extraSpace)
+                && event.getY(1) > centerY - (JOYSTICK_RADIUS+extraSpace)) {
+            if(event.getPointerCount() == 1){
+                points[0].x = (int) event.getX(0);
+                isPushedDown[0] = true;
+            }
+        }*/
         try {
             for (int i = 0; i < event.getPointerCount(); i++) {
-                points[i].x = (int) event.getX(i);
-                points[i].y = (int) event.getY(i);
+                //System.out.println("run "+i);
+
+                points[event.getPointerId(i)].x = (int) event.getX(i);
+                points[event.getPointerId(i)].y = (int) event.getY(i);
 
                 //If a finger is lifted set @isPushedDown[i] to false, so @onDraw won't draw the
                 //lifted finger touch event.
                 if (event.getAction() == MotionEvent.ACTION_UP ||
                         event.getAction() == MotionEvent.ACTION_POINTER_2_UP ||
                         event.getAction() == MotionEvent.ACTION_POINTER_UP) {
-                    isPushedDown[event.getPointerId(i)] = false;
+                    isPushedDown[event.getPointerId(event.getActionIndex())] = false;
                     invalidate();
                 } else {
+                    /*if(event.getPointerCount() == 1){
+                        if (points[0].x < centerX + (JOYSTICK_RADIUS+extraSpace) && points[0].y > centerY - (JOYSTICK_RADIUS+extraSpace)) {
+                            isPushedDown[0] = true;
+                        }
+                        else if(points[0].x > centerX2 - (JOYSTICK_RADIUS+extraSpace) && points[0].y > centerY2 - (JOYSTICK_RADIUS+extraSpace)) {
+                            isPushedDown[1] = true;
+                        }
+                        invalidate();
+                    }
+                    else*/
                     isPushedDown[event.getPointerId(i)] = true;
                     invalidate();
+
                 }
             }
         } catch (ArrayIndexOutOfBoundsException r) {
